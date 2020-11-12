8aa0030fb863643f366d9a1d874ecea861074f23
junkin22 <junkins22@up.edu>
2020-11-11 17:10:38 -0800

    show played cards next to player who played it for human GUI

--- app/src/main/java/edu/up/cs301/chalice/gameStateHearts.java
+++ app/src/main/java/edu/up/cs301/chalice/gameStateHearts.java
@@ -23,8 +23,8 @@ public class gameStateHearts extends GameState {
     private static final long serialVersionUID = 7737393762469851826L;
 
     //instance variables
-    private int p1numCurrentPoints, p2numCurrentPoints, p3numCurrentPoints, p4numCurrentPoints;
-    private int p1RunningPoints, p2RunningPoints, p3RunningPoints, p4RunningPoints;
+    private int p1numCurrentPoints, p2numCurrentPoints, p3numCurrentPoints, p4numCurrentPoints; //points outside of the round/hand
+    private int p1RunningPoints, p2RunningPoints, p3RunningPoints, p4RunningPoints; //points inside the hand
 
     private Deck deck;
 
@@ -412,6 +412,23 @@ public class gameStateHearts extends GameState {
             p3Hand.add(deck.getNextCard());
             p4Hand.add(deck.getNextCard());
         }
+    }
+
+    public ArrayList<Card> getTrickCardsPlayed(){
+        ArrayList<Card> cards = new ArrayList<>();
+        if (p1CardPlayed != null){
+            cards.add(p1CardPlayed);
+        }
+        if (p2CardPlayed != null){
+            cards.add(p2CardPlayed);
+        }
+        if (p3CardPlayed != null){
+            cards.add(p3CardPlayed);
+        }
+        if (p4CardPlayed != null){
+            cards.add(p4CardPlayed);
+        }
+        return cards;
     }
 
     /** Setters for instance variables **/
