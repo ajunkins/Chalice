6c6fe78d69f5da242cd6d0bf30df7a3394221e74
junkin22 <junkins22@up.edu>
2020-11-18 17:55:00 -0800

    added catch for indexoutofbounds exception in PlayerComputerSimple

--- app/src/main/java/edu/up/cs301/chalice/heartsLocalGame.java
+++ app/src/main/java/edu/up/cs301/chalice/heartsLocalGame.java
@@ -271,11 +271,10 @@ public class heartsLocalGame extends LocalGame {
                     if (!isTrickOver()) {
                         state.setWhoTurn(state.getWhoTurn() + 1);
                     }
-
+                    return true;
                 } else {
                     return false;
                 }
-                break;
             case 1:
                 if (isCardValid(state.getP2Hand(), ((ActionPlayCard) action).playedCard())) {
                     state.setP2CardPlayed(((ActionPlayCard) action).playedCard());
@@ -284,10 +283,10 @@ public class heartsLocalGame extends LocalGame {
                     if (!isTrickOver()) {
                         state.setWhoTurn(state.getWhoTurn() + 1);
                     }
+                    return true;
                 } else {
                     return false;
                 }
-                break;
             case 2:
                 if (isCardValid(state.getP3Hand(), ((ActionPlayCard) action).playedCard())) {
                     state.setP3CardPlayed(((ActionPlayCard) action).playedCard());
@@ -296,10 +295,10 @@ public class heartsLocalGame extends LocalGame {
                     if (!isTrickOver()) {
                         state.setWhoTurn(state.getWhoTurn() + 1);
                     }
+                    return true;
                 } else {
                     return false;
                 }
-                break;
             case 3:
                 if (isCardValid(state.getP4Hand(), ((ActionPlayCard) action).playedCard())) {
                     state.setP4CardPlayed(((ActionPlayCard) action).playedCard());
@@ -308,14 +307,13 @@ public class heartsLocalGame extends LocalGame {
                     if (!isTrickOver()) {
                         state.setWhoTurn(0);
                     }
+                    return true;
                 } else {
                     return false;
                 }
-                break;
             default:
                 return false;
         }
-        return true;
     }
 
     /**
@@ -491,7 +489,7 @@ public class heartsLocalGame extends LocalGame {
                 state.setHeartsBroken(true);
             }
             if(isTrickOver()) {
-                sendUpdatedStateTo(players[0]);
+                updateAllPlayers(players);
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
